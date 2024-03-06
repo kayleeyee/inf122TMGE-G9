0a06345de23f18c8d7b8ef30a41958421196d4fe
SPerezdeTudela <sperezde@uci.edu>
2024-03-05 18:42:50 -0800

    Created a run class and grabbed user clicks

--- CreateGrid.py
+++ CreateGrid.py
@@ -1,42 +1,54 @@
 import tkinter as tk
 
-window = tk.Tk()
-
-GRID_WIDTH = 500;
-GRID_HEIGHT = 300;
-
-COL = 5;
-ROWS = 3;
-
-LINE_WIDTH = .5
-
 # Create Grid
-def drawGrid():
-    canvas = tk.Canvas(window, width=GRID_WIDTH, height=GRID_HEIGHT)
+def drawGrid(window : tk.Tk, width, height, num_col, num_row, line_width) -> None:
+    window.geometry = str(width)+"x"+str(height)
+    canvas = tk.Canvas(window, width=width, height=height)
     
      # Draw column lines
-    x0 = GRID_WIDTH/COL
-    for _ in range(COL-1):
-        canvas.create_line(x0,0,x0,GRID_HEIGHT, fill="black", width=LINE_WIDTH)
-        print(x0)
-        x0 += GRID_WIDTH/COL
+    x0 = width/num_col
+    for _ in range(num_col-1):
+        canvas.create_line(x0,0,x0,height, fill="black", width=line_width)
+        x0 += width/num_col
    
     # Draw row lines
-    y0 = GRID_HEIGHT/ROWS
-    for _ in range(ROWS-1):
-        canvas.create_line(0, y0, GRID_WIDTH, y0, fill = "black", width=LINE_WIDTH)
-        y0 += GRID_HEIGHT/ROWS # Get ready for next row
+    y0 = height/num_row
+    for _ in range(num_row-1):
+        canvas.create_line(0, y0, width, y0, fill = "black", width=line_width)
+        y0 += height/num_row # Get ready for next row
 
     canvas.pack()
-
-drawGrid()
 
 # Returns the actual coordinates (x, y) for the canvas
 # Given the location in the matrix
 def findCoord(i : int, j: int):
     pass
 
-window.mainloop()
+# This will pass the input to the 
+# specific game instance 
+def passInput(x: int, y: int):
+    # Will eventually call something
+    # diff than print
+    print(findCoord(x, y))
+
 
 # Take in a matrix and display each square
 # Not sure if we're gonna use actual images or not
+
+def run():
+    window = tk.Tk()
+
+    GRID_WIDTH = 500;
+    GRID_HEIGHT = 300;
+
+    COL = 5;
+    ROWS = 3;
+
+    LINE_WIDTH = .5
+
+    drawGrid(window, GRID_WIDTH, GRID_HEIGHT, COL, ROWS, LINE_WIDTH) # Draw Grid
+    window.bind('<Button-1>', lambda e : passInput(e.x, e.y)) # Handle Events
+
+    window.mainloop()
+
+run()
