819e47285036196d4f7ae002177d95248320f9f6
SPerezdeTudela <sperezde@uci.edu>
2024-03-07 09:33:03 -0800

    Get canvas to return coordinate based on click + fill in matrix

--- CreateGrid.py
+++ CreateGrid.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 # Create Grid
-def drawGrid(window : tk.Tk, width, height, num_col, num_row, line_width) -> None:
+def draw_grid(window : tk.Tk, width, height, num_col, num_row, line_width) -> tk.Canvas:
     window.geometry = str(width)+"x"+str(height)
     canvas = tk.Canvas(window, width=width, height=height)
     
@@ -18,21 +18,39 @@ def drawGrid(window : tk.Tk, width, height, num_col, num_row, line_width) -> Non
         y0 += height/num_row # Get ready for next row
 
     canvas.pack()
+    return canvas
 
 # Returns the actual coordinates (x, y) for the canvas
-# Given the location in the matrix
-def findCoord(i : int, j: int):
-    pass
+# given the location in the matrix
+def matrix_to_canvas(i : int, j: int, b_width, b_height) -> tuple:
+    return (i*b_width, j*b_height)
 
+# Returns the matrix coordinates
+# given the location in the canvas
+# B_width refers to the width of 
+# some grid on the screen
+def canvas_to_matrix(x: int, y: int, b_width, b_height) -> tuple:
+    return (int(x//b_width), int(y//b_height))
+        
 # This will pass the input to the 
 # specific game instance 
-def passInput(x: int, y: int):
+def pass_input(x: int, y: int, b_width, b_height):
     # Will eventually call something
     # diff than print
-    print(findCoord(x, y))
+    i, j = canvas_to_matrix(x, y, b_width, b_height);
+    print(i, j)
 
 
 # Take in a matrix and display each square
+# Let's suppose it's a list of colors for now
+# This can change in the future
+def fill_matrix(arr : list, canvas : tk.Canvas, b_width, b_height) -> None:
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            x, y = matrix_to_canvas(i, j)
+            color = arr[i][j]
+            canvas.create_rectangle(x,y,x+b_width,+b_height, fill=color)    
+
 # Not sure if we're gonna use actual images or not
 
 def run():
@@ -44,10 +62,13 @@ def run():
     COL = 5;
     ROWS = 3;
 
+    BLOCK_W = GRID_WIDTH/COL
+    BLOCK_H = GRID_HEIGHT/ROWS
+
     LINE_WIDTH = .5
 
-    drawGrid(window, GRID_WIDTH, GRID_HEIGHT, COL, ROWS, LINE_WIDTH) # Draw Grid
-    window.bind('<Button-1>', lambda e : passInput(e.x, e.y)) # Handle Events
+    canvas = draw_grid(window, GRID_WIDTH, GRID_HEIGHT, COL, ROWS, LINE_WIDTH) # Draw Grid
+    window.bind('<Button-1>', lambda e : pass_input(e.x, e.y, BLOCK_W, BLOCK_H)) # Handle Events
 
     window.mainloop()
 
