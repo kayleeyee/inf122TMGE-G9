56310c6fe75a212a1bdbdaf7baf6688f04d698dd
Catalina <arreolc2@uci.edu>
2024-03-07 18:02:55 -0800

    Tetris Changes, updated in accordance to game

--- tetris.py
+++ tetris.py
@@ -5,36 +5,74 @@ import random
 import copy
 
 class Tetris(Game):
+    pieces = {
+    "I": [[1, 1, 1, 1]],
+    "L": [[1, 0, 0],
+        [1, 1, 1]],
+    "J": [[0, 0, 1],
+        [1, 1, 1]],
+    "O": [[1, 1],
+        [1, 1]],
+    "S": [[0, 1, 1],
+        [1, 1, 0]],
+    "T": [[0, 1, 0],
+          [1, 1, 1]],
+    "Z": [[1, 1, 0],
+        [0, 1, 1]]
+    }
+
+    color_pieces = {
+        "I" : "RED",
+        "L" : "BLUE",
+        "J" : "YELLOW",
+        "O" : "PURPLE",
+        "S" : "GREEN",
+        "T" : "PINK",
+        "Z" : "GREY" 
+    }
+    
     GAME_NAME = 'Tetris'
     TETRIS_ROWS = 10
     TETRIS_COLS = 20
 
+    current_piece_key = None
+    current_piece = None
+    current_piece_color = None
+    current_piece_start_row = 0
+    current_piece_start_col = 0
+    
+    
+    piece_keys = list(pieces.keys())
+
+    grid = Grid(TETRIS_ROWS, TETRIS_COLS)
+    
     #grid = None
 
     def __init__(self, players) -> None:
-        super().__init__(self.TETRIS_ROWS, self.TETRIS_COLS, self.GAME_NAME, players)
         print("Tetris Innit")
         self.runGameLoop()
         
     def populate_initial_grid(self):
-        print("Populate_initial_grid")
-        print(self.grid.return_grid()[0][0])
+        self.addNewPieces()
     
     def addNewPieces(self):
         piece, piece_key, start_row, start_col = self._choose_new_pieces()
+        self.current_piece = piece
+        self.current_piece_key = piece_key
+        self.current_piece_color = self.color_pieces[self.current_piece_key]
+        self.current_piece_start_row = start_row
+        self.current_piece_start_col = start_col
 
         if (self.endGame != False):    
         #if not end game, add the piece :D, this is simple one??
             for row in range(len(piece)):
                 for col in range(len(piece[row])):
                     if piece[row][col] == 1:           
-                        self.grid.matrix[start_row + row][start_col + col] = Color(color_pieces[piece_key])
-
-        return piece, piece_key, start_row, start_col     
+                        self.grid.matrix[start_row + row][start_col + col] = Color(self.current_piece_color)   
     
-    def _rotate(self, piece, piece_key, start_row, start_col):
+    def _rotate(self):
         #modify the piece's list and store in a variable
-        rotated_piece = list(zip(*piece[::-1]))
+        rotated_piece = list(zip(*self.current_piece[::-1]))
 
         new_start_col = (self.TETRIS_COLS - len(rotated_piece[0])) // 2
 
@@ -42,69 +80,61 @@ class Tetris(Game):
         temp_grid = copy.deepcopy(self.grid)
 
 
-        for row in range(len(piece)):
-            for col in range(len(piece[row])):
-                if piece[row][col] == 1:
-                    temp_grid.matrix[start_row + row][start_col + col] = Color("COLORLESS")
+        for row in range(len(self.current_piece)):
+            for col in range(len(self.current_piece[row])):
+                if self.current_piece[row][col] == 1:
+                    temp_grid.matrix[self.current_piece_start_row + row][self.current_piece_start_col + col] = Color("COLORLESS")
         
         #if there is not collision, rotate the piece
-        if (self._check_collison(rotated_piece, temp_grid, start_row, new_start_col) != True):
+        if (self._check_collison(rotated_piece, temp_grid, self.current_piece_start_row, new_start_col) != True):
             for row in range(len(rotated_piece)):
                 for col in range(len(rotated_piece[row])):
                     if rotated_piece[row][col] == 1:
-                        temp_grid.matrix[start_row + row][new_start_col + col] = Color(color_pieces[piece_key])
-            grid = temp_grid
-            return rotated_piece, piece_key, start_row, new_start_col
-        
-        return piece, piece_key, start_row, start_col
-    
-    def _move_down(self, piece, piece_key, start_row, start_col):
-        if (self._check_collison(piece, self.grid, start_row + 1, start_col) != True): #??
-            temp_row = start_row + 1
+                        temp_grid.matrix[self.current_piece_start_row + row][new_start_col + col] = Color(self.current_piece_color)
+            self.grid = temp_grid
 
-            for row in range(len(piece)):
-                for col in range(len(piece[row])):
-                    if piece[row][col] == 1:
-                        self.grid.matrix[start_row + row][start_col + col] = Color("COLORLESS") #might wanna do these two in seperate
-                        self.grid.matrix[temp_row + row][start_col + col] = Color(color_pieces[piece_key]) # loops statement if wanna update grid 
-                                                                                                            #after each loopfor presentation
+            self.current_piece = rotated_piece
+            self.current_piece_start_row = new_start_col
+    
+    def _move_down(self):
+        while (self._check_collison(self.current_piece, self.grid, self.current_piece_start_col + 1, self.current_piece_start_row) != True): #??
+            temp_row = self.current_piece_start_row + 1
+
+            for row in range(len(self.current_piece)):
+                for col in range(len(self.current_piece[row])):
+                    if self.current_piece[row][col] == 1:
+                        self.grid.matrix[self.current_piece_start_row + row][self.current_piece_start_col + col] = Color("COLORLESS")
+                        self.grid.matrix[temp_row + row][self.current_piece_start_col + col] = Color(self.current_piece_color)
             
             start_row += 1
-        return piece, piece_key, start_row, start_col
 
-    def _move_left(self, piece, piece_key, start_row, start_col):
-        if (self._check_collison(piece, self.grid, start_row, start_col - 1) != True): #??
-            temp_col = start_col - 1
+    def _move_left(self):
+        if (self._check_collison(self.current_piece, self.grid, self.current_piece_start_row, self.current_piece_start_col - 1) != True): #??
+            temp_col = self.current_piece_start_col - 1
 
-            for row in range(len(piece)):
-                for col in range(len(piece[row])):
-                    if piece[row][col] == 1:
-                        self.grid.matrix[start_row + row][start_col + col] = Color("COLORLESS") #might wanna do these two in seperate
-                        self.grid.matrix[start_row + row][temp_col + col] = Color(color_pieces[piece_key]) # loops statement if wanna update grid 
-                                                                                                            #after each loopfor presentation
+            for row in range(len(self.current_piece)):
+                for col in range(len(self.current_piece[row])):
+                    if self.current_piece[row][col] == 1:
+                        self.grid.matrix[self.current_piece_start_row + row][self.current_piece_start_col + col] = Color("COLORLESS")
+                        self.grid.matrix[self.current_piece_start_row + row][temp_col + col] = Color(self.current_piece_color)
             
-            start_col -= 1
-        return piece, piece_key, start_row, start_col
+            self.current_piece_start_col -= 1
 
-    def _move_right(self, piece, piece_key, start_row, start_col):
-        if (self._check_collison(piece, self.grid, start_row, start_col + 1) != True): #??
-            temp_col = start_col + 1
+    def _move_right(self):
+        if (self._check_collison(self.current_piece, self.grid, self.current_piece_start_row, self.current_piece_start_col + 1) != True): #??
+            temp_col = self.current_piece_start_col + 1
 
-            for row in range(len(piece)):
-                for col in range(len(piece[row])):
-                    if piece[row][col] == 1:
-                        self.grid.matrix[start_row + row][start_col + col] = Color("COLORLESS") #might wanna do these two in seperate
-                        self.grid.matrix[start_row + row][temp_col + col] = Color(color_pieces[piece_key]) # loops statement if wanna update grid 
-                                                                                                            #after each loopfor presentation
+            for row in range(len(self.current_piece)):
+                for col in range(len(self.current_piece[row])):
+                    if self.current_piece[row][col] == 1:
+                        self.grid.matrix[self.current_piece_start_row + row][self.current_piece_start_col + col] = Color("COLORLESS")
+                        self.grid.matrix[self.current_piece_start_row + row][temp_col + col] = Color(self.current_piece_color)
             
-            start_col += 1
-        return piece, piece_key, start_row, start_col
+            self.current_piece_start_col += 1
 
     def _choose_new_pieces(self):
-        piece_keys = list(pieces.keys())
-
-        new_piece_key = random.choice(piece_keys)
-        new_piece = pieces[new_piece_key]
+        new_piece_key = random.choice(self.piece_keys)
+        new_piece = self.pieces[new_piece_key]
 
         start_row = 0
         start_col = (self.TETRIS_COLS - len(new_piece[0])) // 2
@@ -145,42 +175,3 @@ class Tetris(Game):
 
                 grid = [[Color("COLORLESS") for _ in range(self.TETRIS_COLS)]] + slice1 + slice2 #??
 
-pieces = {
-    "I": [
-        [1, 1, 1, 1]
-    ],
-    "L": [
-        [1, 0, 0],
-        [1, 1, 1]
-    ],
-    "J": [
-        [0, 0, 1],
-        [1, 1, 1]
-    ],
-    "O": [
-        [1, 1],
-        [1, 1]
-    ],
-    "S": [
-        [0, 1, 1],
-        [1, 1, 0]
-    ],
-    "T": [
-        [0, 1, 0],
-        [1, 1, 1]
-    ],
-    "Z": [
-        [1, 1, 0],
-        [0, 1, 1]
-    ]
-}
-
-color_pieces = {
-    "I" : "RED",
-    "L" : "BLUE",
-    "J" : "YELLOW",
-    "O" : "PURPLE",
-    "S" : "GREEN",
-    "T" : "PINK",
-    "Z" : "GREY" 
-}
